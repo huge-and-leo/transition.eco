02f42b7e2ebef346c569f4aa698300fa07cde078
Eugene <kse3@bfh.ch>
2023-11-10 12:19:01 +0100

    Style new menu toggler and combine both primary and secondary menus

--- themes/transition.eco/assets/js/main.js
+++ themes/transition.eco/assets/js/main.js
@@ -2,10 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const primaryNavToggle = document.querySelector(".primary-nav-toggle");
     primaryNavToggle.addEventListener("click", _ => {
         document.body.classList.toggle("primary-nav--opened");
-    })
-
-    const secondaryNavToggle = document.querySelector(".secondary-nav-toggle");
-    secondaryNavToggle.addEventListener("click", _ => {
-        document.body.classList.toggle("secondary-nav--opened");
+        primaryNavToggle.classList.toggle("primary-nav-toggle--opened");
     })
 });
